818c3687205264cba5b491597290796eb730182e
Teemu Puukko <teemuki@gmail.com>
2019-04-09 20:25:06 +0300

    number-input component

--- DartPointCalculator/index.js
+++ DartPointCalculator/index.js
@@ -9,11 +9,63 @@ function isNumber(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
+Vue.component('player', {
+  template: '#player-template'
+});
+
+Vue.component('number-input', {
+  template: '#number-input-template',
+  
+  props: ['value'],
+
+  data: function() {
+    return {
+      point: 0
+    };
+  },
+
+  watch: {
+    point: function() {
+      this.$emit('point', this.value, this.point);
+    }
+  },
+
+  computed: {
+
+    /**
+     * Validate values that can be scored in different stages of the game.
+     * This is used only show visual clue that something is wrong with
+     * inputted value. This won't affect what you can write to the field.
+     */
+    errors: function() {
+
+      const point = this.point;
+      const baseScores = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]; 
+
+      return {
+        points:{
+          "17": !([0, 17, 17*2, 17*3].includes(point)),
+          "d":  !( baseScores.map( v => v*2).includes(point)),
+          "18": !([0, 18, 18*2, 18*3].includes(point)),
+          "t": !(baseScores.map( v => v*3).includes(point)),
+          "19": !([0, 19, 19*2, 19*3].includes(point)),
+          "r": !([0, 41, 82, 123 ].includes(point)),
+          "20": !([0, 20, 20*2, 20*3].includes(point)),
+          "b": !([0, 25, 50, 75, 100, 125, 150].includes(point))
+        }
+      };      
+    }
+  }
+
+});
+
 var app = new Vue({
 
   el: '.forzacalculator',
 
   data: {
+
+    playerName: 'Teemu',
 
     points: {
       "17": 0,
@@ -30,8 +82,8 @@ var app = new Vue({
 
 
   methods: {
-    inputClicked: function( value ) {
-      console.log( ":D:D:D:D")
+    pointChanged: function( value, points ) {
+      this.points[value] = points;
     }
   },
 
@@ -65,4 +117,7 @@ var app = new Vue({
       },0);
     }
   }
+  
 });
+
+
